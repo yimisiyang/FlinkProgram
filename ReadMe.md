d7bcd74b60314b8acda09828bedf20f84feeee7c
yimisiyang <15230034878@163.com>
2021-11-02 21:39:38 +0800

    add:跑通standalone模式作业提交

--- ReadMe.md
+++ ReadMe.md
@@ -18,3 +18,60 @@ Exception in thread "main" java.lang.NullPointerException: Cannot find compatibl
 ```
 
 问题原因是： `flink-streaming-scala_2.12` 依赖的作用域不对。
+
+# 2. flink流处理时设置并行度
+
+```java
+StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+env.setParallelism(8); //默认以计算机CPU核数作为并行度
+```
+
+# 3. flink部署安装
+
+阿里云的opt目录下已部署完成
+
+# 4. nc命令
+
+```shell
+nc -lk 7777      #用来发socket数据流
+```
+
+# 5. slot太小导致跑不起来
+
+将flink配置文件中的以下参数改为4（推荐设置为CPU的核心数）
+
+```
+taskmanager.numberOfTaskSlots: 4
+```
+
+集群资源太少，需要的资源太多导致运行不起来
+
+1）调大集群资源比如改（taskmanager.numberOfTaskSlots: 4）
+
+2）将job的并行度调低
+
+# 6. 提交作业的方式
+
+1）通过web页面提交
+
+2）命令行提交命令
+
+```shell
+# 提交作业
+./bin/flink run -c com.tj712.wc.StreamWordCount -p 3 F:\Program\FlinkProgram\src\main\java\com\tj712\wc\StreamWordCount.java
+# 查看运行的作业
+./bin/flink list
+# 查看所有作业
+./bin/flink list -a
+# 取消某个作业
+./bin/flink cancel 59868f5e539c7322f876c517986ff375 # 取消调某个任务，cancel后跟任务id
+```
+
+# 7. Yarn 模式
+
+以yarn模式部署flink任务时，要求flink是有Hadoop支持的版本，Hadoop环境需要保证版本在2.2以上，并且集群中安装有HDFS服务。
+
+flink提供了两种在yarn上运行的模式，分别为Session-Cluster和Per-Job-Cluster模式。
+
+
+
--- src/main/java/com/tj712/wc/StreamWordCount.java
+++ src/main/java/com/tj712/wc/StreamWordCount.java
@@ -2,6 +2,7 @@ package com.tj712.wc;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -17,10 +18,17 @@ public class StreamWordCount {
     public static void main(String[] args) throws Exception {
         //创建流处理执行环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        //从文件中读取
-        String inputPath = "F:\\Program\\FlinkProgram\\src\\main\\resources\\Hello.txt";
-        DataStreamSource<String> inputDataStream = env.readTextFile(inputPath);
+        //1.从文件中读取
+//        String inputPath = "F:\\Program\\FlinkProgram\\src\\main\\resources\\Hello.txt";
+//        DataStreamSource<String> inputDataStream = env.readTextFile(inputPath);
 
+        //parameter tool工具从程序启动参数中提取配置项,args来源与main方法中的参数
+//        ParameterTool parameterTool = ParameterTool.fromArgs(args);
+//        String host = parameterTool.get("host");
+//        int port = parameterTool.getInt("port");
+
+        //2. 从socket文本流中读取数据
+        DataStreamSource<String> inputDataStream = env.socketTextStream("182.92.57.11",7777);
         //基于数据流进行转换计算
         SingleOutputStreamOperator<Tuple2<String, Integer>> resultStream = inputDataStream.flatMap(new WordCount.MyFlatMapper())
                 .keyBy(0)
