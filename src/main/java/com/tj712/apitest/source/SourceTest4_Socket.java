5d517ba2c5701c3fc9ecdec4bfc7ed07c08e2185
yimisiyang <15230034878@163.com>
2021-11-05 10:54:42 +0800

    add: 增加socket数据源Demo

---
+++ src/main/java/com/tj712/apitest/source/SourceTest4_Socket.java
@@ -0,0 +1,45 @@
+package com.tj712.apitest.source;
+
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Date: 2021/11/5
+ * @Time: 10:08
+ * @author: ThinkPad
+ */
+public class SourceTest4_Socket {
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<Tuple2<String, Integer>> dataStream = env
+                .socketTextStream("182.92.57.11", 7777)
+                .flatMap(new Splitter())
+                .keyBy(value -> value.f0)
+                .window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
+                .sum(1);
+
+        dataStream.print();
+
+        env.execute("Window WordCount");
+    }
+
+    public static class Splitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+        @Override
+        public void flatMap(String sentence, Collector<Tuple2<String, Integer>> out) throws Exception {
+            for (String word: sentence.split(" ")) {
+                out.collect(new Tuple2<String, Integer>(word, 1));
+            }
+        }
+    }
+}
